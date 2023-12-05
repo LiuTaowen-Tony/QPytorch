d2ae9ed9ac7c7580e3b78084479bf8017a2f2dd0
Tony T Liu <tl2020@ee-kraken.ee.ic.ac.uk>
2023-12-05 17:27:46 +0000

    upload result

--- run_mp_train_exp.sh
+++ run_mp_train_exp.sh
@@ -5,24 +5,25 @@ source .venv/bin/activate
 
 for man_width in 3; do
 for round2 in stochastic nearest ; do
-for loss_scale in 1 8 64 256 1024; do
+for loss_scale in 256 1024 2048 4096; do
 for batch_size in 64; do
 for lr in 0.05; do
-for exp_width in 2 3 5 8; do
+for bk_exp_width in 3 4; do
+for fw_exp_width in 2 3 4; do
 for run in 0; do
 	device=0
-	if [ $exp_width -lt 4 ] 
+	if [ $bk_exp_width -lt 4 ] 
 	then
 		device=1
 	fi
 	CUDA_VISIBLE_DEVICES=$device python mix_precision_train.py \
 		-w $man_width -e $man_width -g $man_width -a $man_width \
-		--weight-ew $exp_width \
-		--error-ew $exp_width \
-		--gradient-ew $exp_width \
-		--activation-ew $exp_width \
+		--weight-ew $fw_exp_width \
+		--error-ew $bk_exp_width \
+		--gradient-ew $fw_exp_width \
+		--activation-ew $bk_exp_width \
 		--loss-scale $loss_scale \
-		--log-path loss_scale_log \
+		--log-path loss_scale_finegrain_log \
 		--epochs 150 \
 		--weight-round $round2 \
 		--error-round $round2 \
@@ -34,10 +35,11 @@ for run in 0; do
 		--mix-precision True &
 done
 done
+done
 	wait
 done
 done
 done
 done
-done
+done 
 
